6ba8c41df7b80bc1838c124539ba813e4b691773
Ferla04 <fer.velas135@gmail.com>
2023-02-12 05:39:02 -0500

    creacion del home

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -4,9 +4,16 @@ export const Home = () => {
   return (
     <section id='home' className='home'>
       <article className='home-info'>
-        <div className='neon'>Fer<span id='offset'>n</span>anda</div>
+        <div className='home-welcome'>
+          <p>Hola! soy</p>
+          <h3 className='neon'>Fer<span id='offset'>n</span>anda velasquez</h3>
+        </div>
         <p>Mollit nisi tempor nostrud deserunt cillum. Ut pariatur fugiat velit ut anim fugiat quis cillum aliquip amet ex consectetur mollit voluptate. Sint laborum aute nulla aute aute labore ipsum.</p>
         <div className='home-networks'>
+          <a className='cv' target='_blank' href='/CV-Fernanda_Velasquez.pdf'>
+            <i className='fa-solid fa-user' />
+            CV
+          </a>
           <a className='fa-brands fa-github' href='' />
           <a className='fa-brands fa-linkedin' href='' />
         </div>
