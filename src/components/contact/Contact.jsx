8faca5916a555bfc5c95655b3e688ff47a8d68d0
Ferla04 <fer.velas135@gmail.com>
2023-02-14 13:06:44 -0500

    creacion de contact

---
+++ src/components/contact/Contact.jsx
@@ -0,0 +1,15 @@
+import { getRouteById } from '../../helpers/getRouteById'
+import './contact.css'
+
+export const Contact = () => {
+  const { id, name: title } = getRouteById('contact')
+
+  return (
+    <section id={id} className='contact'>
+      <h2>{title}</h2>
+      <p>Aliquip proident exercitation proident occaecat officia qui aliquip est aliquip duis.</p>
+      <a href='mailto:fer.velas135@gmail.com'>Escribeme</a>
+      <img src='/assets/sayHi.png' alt='' />
+    </section>
+  )
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,5 @@
 export * from './aboutMe/AboutMe'
+export * from './contact/Contact'
 export * from './main/Home'
 export * from './navbar/Navbar'
 export * from './projects/Projects'
