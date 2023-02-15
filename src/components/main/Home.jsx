3a909d520c4b904ea69d5c7c83e49fc73f824d5a
Ferla04 <fer.velas135@gmail.com>
2023-02-15 00:41:12 -0500

    responsive - 480px

--- src/components/main/Home.jsx
+++ src/components/main/Home.jsx
@@ -8,6 +8,7 @@ export const Home = () => {
           <p>Hola! soy</p>
           <h3 className='neon'>Fer<span id='offset'>n</span>anda velasquez M.</h3>
         </div>
+        <img src='/assets/heyPhone.png' alt='me' />
         <p>Mollit nisi tempor nostrud deserunt cillum. Ut pariatur fugiat velit ut anim fugiat quis cillum aliquip amet ex consectetur mollit voluptate. Sint laborum aute nulla aute aute labore ipsum.</p>
         <div className='home-networks'>
           <a className='cv' target='_blank' href='/CV-Fernanda_Velasquez.pdf'>
