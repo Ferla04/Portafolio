7effbd28c2818b98cae3e876b6efead38306ec33
Ferla04 <fer.velas135@gmail.com>
2023-02-15 03:10:51 -0500

    problema con las imagenes

--- src/components/projects/ProjectsItem.jsx
+++ src/components/projects/ProjectsItem.jsx
@@ -4,7 +4,7 @@ export const ProjectsItem = ({ name, img, repo, link, info }) => {
     <section className='galery-item'>
       <h3 translate='no'>{name}</h3>
       <article className='galery-img'>
-        <img src={`/assets/${img}`} alt={name} />
+        <img src={`/public/assets/${img}`} alt={name} />
         <div className='galery-icon'>
           <a href={repo} target='_blank' className='fa-brands fa-github' rel='noreferrer' />
           <a href={link} target='_blank' className='fa-solid fa-earth-americas' rel='noreferrer' />
